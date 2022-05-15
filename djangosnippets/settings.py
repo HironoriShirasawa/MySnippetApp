ce4b476edd23eb808b415bc1d847c041ef415fc0
hiro <kaibonpoyopoyon@outlook.jp>
2022-05-15 14:54:55 +0900

    first modified

--- djangosnippets/settings.py
+++ djangosnippets/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'snippets.apps.SnippetsConfig',
+    'django_bootstrap5',
+    'pygments_renderer',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'djangosnippets.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- snippets/views.py
+++ snippets/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 
 from .models import Snippet
+from .forms import SnippetForm
 # Create your views here.
 
 def top(request):
@@ -9,11 +12,35 @@ def top(request):
   context = {"snippets": snippets}
   return render(request, "snippets/top.html", context)
 
+@login_required
 def snippet_new(request):
-  return HttpResponse('スニペットの登録')
+  if request.method == 'POST':
+    form = SnippetForm(request.POST)
+    if form.is_valid():
+      snippet = form.save(commit=False)
+      snippet.created_by = request.user
+      snippet.save()
+      return redirect(snippet_detail, snippet_id=snippet.pk)
+  else:
+    form = SnippetForm()
+  return render(request, "snippets/snippet_new.html", {'form': form})
 
-def snippet_edit(request):
-  return HttpResponse('スニペットの編集')
+@login_required
+def snippet_edit(request, snippet_id):
+  snippet = get_object_or_404(Snippet, pk=snippet_id)
+  if snippet.created_by_id != request.user.id:
+    return HttpResponseForbidden("このスニペットの編集は許可されていません。")
+  
+  if request.method == "POST":
+    form = SnippetForm(request.POST, instance=snippet)
+    if form.is_valid():
+      form.save()
+      return redirect('snippet_detail', snippet_id=snippet_id)
+  else:
+    form = SnippetForm(instance=snippet)
+  return render(request, 'snippets/snippet_edit.html', {'form':form})
+
+
 
 def snippet_detail(request, snippet_id):
   snippet = get_object_or_404(Snippet, pk=snippet_id)
